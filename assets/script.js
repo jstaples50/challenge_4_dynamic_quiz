41b752dbee19da4911c3874d7d92c60c2cfd6439
Jstaples <jstaplesmusic@gmail.com>
2022-09-21 21:29:53 -0400

    Created checkIfCorrect function to determine if li clicked is correct.

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,60 @@
 // Global variables
 
 var questionEl = document.querySelector("#question-tag");
+var answerDivEl = document.querySelector("#answers");
 var answerListEl = document.querySelector("#answer-list");
 
 
 var question1 = {
     prompt: "Question 1 is asked here",
-    answerArray: ["A", "B", "C", "D"]
+    answerArray: ["A", "B", "C", "D"],
+    correctAnswer: "A"
 }; 
 
 var question2 = {
     prompt: "Question 2 is asked here",
-    answerArray: ["A", "B", "C", "D"]
+    answerArray: ["A", "B", "C", "D"],
+    correctAnswer: "B"
 }; 
 
 var question3 = {
     prompt: "Question 3 is asked here",
-    answerArray: ["A", "B", "C", "D"]
+    answerArray: ["A", "B", "C", "D"],
+    correctAnswer: "C"
 }; 
 
 questionArray = [question1, question2, question3];
 
 // Question is rendered to DOM
 
-function renderQuestion(){
-    questionEl.textContent = question1.prompt;
-    for (i = 0; i < question1.answerArray.length; i++) {
+function renderQuestion(question) {
+    questionEl.textContent = question.prompt;
+    for (i = 0; i < question.answerArray.length; i++) {
         var liEl = document.createElement("li");
         liEl.setAttribute("data-number", i + 1);
-        liEl.textContent = question1.answerArray[i];
+        liEl.textContent = question.answerArray[i];
         answerListEl.appendChild(liEl);
     }
+
+    // Check if answer clicked is correct
+    // Click event listener added
+    //  *TODO* : trueResponse textContent keeps adding on after element is pushed
+
+    function checkIfCorrect(event) {
+        let hr = document.createElement("hr");
+        let trueResponse = document.createElement("p");
+        trueResponse.style.fontStyle = "italic";
+        answerDivEl.appendChild(hr);
+        answerDivEl.appendChild(trueResponse);
+        if (event.target.textContent === question.correctAnswer) {
+            trueResponse.textContent = "That is correct!"
+        } else { 
+            trueResponse.textContent = "Sorry, that is wrong..."
+        }
+    }
+
+    answerListEl.addEventListener('click', checkIfCorrect);
 }
 
-renderQuestion();
+
+renderQuestion(questionArray[1]);
